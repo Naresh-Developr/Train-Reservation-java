b4591bb3d7b1c9386af82e0bd76965bab905fbd4
Naresh-Developr <naresh.k.personal@gmail.com>
2024-03-22 13:10:00 +0530

    Main class

--- trainReservation.java
+++ trainReservation.java
@@ -42,43 +42,101 @@ public class trainReservation{
 
         Scanner sc = new Scanner(System.in);
 
-        public int pnrNumber(){
+        public int getPnrNumber(){
             Random random = new Random();
             pnrNumber = random.nextInt(max) + min;
             return pnrNumber;
         }
 
-        public String passengeName(){
+        public String getPassengeName(){
             System.out.println("Enter passenger Name: ");
             passengerName = sc.nextLine();
             return passengerName;
         }
-        public String trainNumber(){
+        public String getTrainNumber(){
             System.out.println("Enter Train Number: ");
             trainNumber = sc.nextLine();
             return trainNumber;
         }
-        public String classType(){
+        public String getClassType(){
             System.out.println("Enter class type: ");
             classtype = sc.nextLine();
             return classtype;
         }
-        public String journy(){
+        public String getJourny(){
             System.out.println("Enter journy: ");
             journy = sc.nextLine();
             return journy;
         } 
-        public String from(){
+        public String getFrom(){
             System.out.println("Enter From: ");
             from = sc.nextLine();
             return from;
         }
-        public String to(){
+        public String getTo(){
             System.out.println("Enter To : ");
             to = sc.nextLine();
             return to;
         }
             
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        user u1 = new user();
+        String username = u1.getUsername();
+        String password = u1.getPassword();
+
+        String Url = "jdbc:mysql://localhost:3306/task1";
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try(Connection myConnection = DriverManager.getConnection(Url, username, password)){
+                System.out.println("Connection Granted");
+                while(true){
+                    String insertRecord = "insert into reservation value(?,?,?,?,?,?,?)";
+                    String deleteRecord = "Delete from reservation where pnr_number = ?";
+                    String ShowQuery = "SELECT * FROM reservation";
+
+                    System.out.println("Enter Choice: ");
+                    System.out.println("1. Insert Record\n");
+                    System.out.println("2. Delete Record\n");
+                    System.out.println("3. Show all Records\n");
+                    System.out.println("4. Exit\n");
+                    int choice  = sc.nextInt();
+
+                    if(choice == 1){
+                        pnrRecords pnr = new pnrRecords();
+
+                        int pnr_number = pnr.getPnrNumber();
+                        String trainNumber = pnr.getTrainNumber();
+                        String passengerName = pnr.getPassengeName();
+                        String classtype = pnr.getClassType();
+                        String journy = pnr.getJourny();
+                        String from = pnr.getFrom();
+                        String to = pnr.getTo();
+
+                        try(PreparedStatement preparedStatement = Connection.prepareStatement(insertRecord)){
+
+                        }catch(Exception e){
+
+                        }
+                    }
+
+
+                    
+
+                }
+
+            }catch(Exception e){
+
+            };
+
+        }catch(Exception e){
+
+        };
+
     }
     
 }
